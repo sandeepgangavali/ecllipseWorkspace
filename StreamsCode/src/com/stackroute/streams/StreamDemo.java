04b6a46ed6b2ac24e070dc801a694076bc3dd5ef
P@ssw0rd <sandeep.gangavali@itcinfotech.com>
2020-04-01 18:25:45 +0530

    JavaCode

---
+++ StreamsCode/src/com/stackroute/streams/StreamDemo.java
@@ -0,0 +1,20 @@
+package com.stackroute.streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamDemo {
+	
+	public static void main(String[] args)
+	{
+		List<List<String>> namesNested=Arrays.asList(
+		Arrays.asList("San","Ram"),	
+		Arrays.asList("Jhon","Cena"),
+		Arrays.asList("Big","Show")	);
+				
+				
+
+	}
+
+}
